04922de42206bbfc1466e429d2bc244dfe6f4259
Rebeca <rebeca@hologram.io>
2021-05-08 18:10:33 -0500

    FLASK API for fetching data

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify,
+from flask import Flask,request,jsonify
 from flask_restful import Resource, Api
 import requests
 import requests_cache
@@ -14,7 +14,7 @@ parser.read("config.txt")
 
 email = parser.get("config", "email")
 apikey = parser.get("config", "apikey")
-base = parser.get("config", "apiurl")
+BASE = parser.get("config", "apiurl")
 
 # ==============================#
 #           CONSTANTS           #
@@ -23,7 +23,7 @@ base = parser.get("config", "apiurl")
 
 # URLS
 PARAM_ENDPOINT_URL = f"https://aqs.epa.gov/data/api/list/parametersByClass?email={email}&key={apikey}"
-COUNTY_ANNUAL_SUMMARY_URL = f"https://aqs.epa.gov/data/api/sampleData/byCounty?email={email}&key={apikey}&param={param}&bdate={bdate}&edate={edate}&state=06&county={county}"
+COUNTY_ANNUAL_SUMMARY_URL = f"https://aqs.epa.gov/data/api/sampleData/byCounty?email={email}&key={apikey}&param={param}&bdate={bdate}&edate={edate}&state=06"
 
 # CODES
 
@@ -37,56 +37,31 @@ STATE = '06'
 # 	Sacramento: 067
 
 COUNTY_LIST = ['023','037','075','073','067']
-# Parameters
-# {
-# "code": "11101",
-# "value_represented": "Suspended particulate (TSP)"
-# },
-# {
-#       "code": "11114",
-#       "value_represented": "Windblown particulate"
-#     },
-# { "code": "11203",
-# "value_represented": "Light scatter"
-# },
-# {
-# "code": "11204",
-# "value_represented": "Smoke"
-# },
-# {
-#       "code": "42101",
-#       "value_represented": "Carbon monoxide"
-#     },
-# {
-#     "code": "25101",
-#     "value_represented": "Dustfall Combustible (SP)"
-# },
-#
-# {
-#     "code": "31101",
-#     "value_represented": "Respirable part"
-# },
-# {
-#     "code": "42401",
-#     "value_represented": "Sulfur dioxide"
-# },
-# {
-#     "code": "65305",
-#     "value_represented": "Radioactivity rainfall"
-# },
-# {
-#     "code": "68110",
-#     "value_represented": "Relative Humidity"
-# },
-# {
-#     "code": "66101",
-#     "value_represented": "Cloud cover"
-# },
 
-PARAM_LIST = ["11101", "11114","11203",
-              "11204", "42101", "25101",
-              "31101", "42401", "65305",
-              "68110", "66101"]
+# Parameters
+# ------------
+# Code, Value Represented
+# "42101","Carbon monoxide"
+# "42401","Sulfur dioxide"
+# "42602","Nitrogen dioxide (NO2)"
+# "44201","Ozone"
+# "81102","PM10 Total 0-10um STP"
+# "88101","PM2.5 - Local Conditions"
+# "88502","Acceptable PM2.5 AQI & Speciation Mass"
+# "12403","Sulfate (TSP) STP"
+# "43102","Total NMOC (non-methane organic compound)"
+# "62101","Outdoor Temperature"
+# "63301","Solar radiation"
+# "64101","Barometric pressure"
+# "65102","Rain/melt precipitation"
+# "81102","PM10 Total 0-10um STP"
+# "85101","PM10 - LC"
+# "86101","PM10-2.5 - Local Conditions"
+# "86502","Acceptable PM10-2.5 - Local Conditions"
+
+
+PARAM_LIST = ["42101", "42401","42602","44201", "62101", "65102", "811202","86101", "86502",
+"88101","88502"]
 
 
 # ============================== #
@@ -114,8 +89,153 @@ def list_params(filter_by=None):
 
 
 
-@app.route('/annual/<int:county>')
-def show_annual_summary_by_county(county):
+def __get_url_county_x_param(county, date_arg):
+    '''
+    Get the url string uniue to a combo of county and date arg string
+    :param county: county code
+    :param date_arg: formatted str
+    :return: full url string for api GET request
+    '''
+
+    url = BASE + date_arg + f"&state=06&county={county}"
+
+    return url
+
+def __generate_date_args(param, start_year=None, end_year=None):
+
+    '''
+    This just generates the correctly formatted date arg to pass.
+    Years start on Jan 1
+    :param start_year: YY (e.g. 2001 = 01)
+    :param end_year: YY (max 21)
+    :return: array of strings
+    '''
+
+    if end_year:
+        assert(end_year < 2021)
+    else:
+        end_year = 2021
+
+    if start_year is None:
+        start_year = 2001
 
 
-    response = requests.get()
+    diff = range(start_year, end_year)
+    all_years = list(diff)
+
+    date_formatting_pattern: f"'010120{YY}'"
+    args = []
+    for y in all_years:
+        new_arg = date_formatting_pattern(YY=y)
+        args.append(new_arg)
+
+
+    full_args = []
+    for a in args:
+        add_to_arg = f"&param={param}&bdate={start_year}&edate={a}&state=06"
+        full_arg = BASE + add_to_arg
+        full_args.append(full_arg)
+
+    return full_args
+
+
+def pull_annual(all_requests_gen):
+    '''
+
+    :param all_requests_gen:
+    :return: array of objects (JSON response data)
+    '''
+
+    all_request_responses = []
+    for a in all_requests_gen:
+        response_body = get_annual_summary(a)
+        all_request_responses.append(response_body)
+
+    return all_request_responses
+
+
+def _generate_all_annual_requests_less_county(p_list = None,
+                                     bdate = None,
+                                     edate = None):
+
+    '''
+
+    :param p_list:
+    :param bdate:
+    :param edate:
+    :return: list of strings
+    '''
+    if p_list:
+        ps = p_list
+
+    else:
+        ps = PARAM_LIST
+
+    if bdate:
+        b = bdate
+
+    else:
+        b = "19990101"
+
+    if edate:
+        e = edate
+    else:
+        e = "20210101"
+
+
+    # I am sincerely sorry for the heinous amount of for loops in this software but hey, hack, right?
+
+
+    almost_full_urls = []
+    for p in ps:
+        url_slice = f"&param={p}&bdate={b}&edate={e}"
+        full_url_less_county = BASE + url_slice
+        almost_full_urls.append(full_url_less_county)
+
+    return almost_full_urls
+
+
+def _generate_all_annual_requests(a_list):
+
+    request_endpoints = []
+    for a in a_list:
+        for c in COUNTY_LIST:
+            url = a + f"&county={c}"
+            request_endpoints.append(url)
+
+    return request_endpoints
+
+
+
+
+
+# WRAPPER TO GET ALL ANNUAL REPORTS - USE SPARINGLY
+
+# @TODO: when I get back from break - implement async io logic stuff to do all annual gets at once(threaded)
+# relies on client session apparently
+
+@app.route('/do_annual')
+def get_annual_summary(request_endpoints):
+
+    response = requests.get(request_endpoints)
+
+    return response.text
+
+
+
+
+
+
+
+
+
+# # Pull all annual reports from the counties in our county list
+# # pass specific param
+# @app.route('annual_all/<int:param code>')
+# def pull_county_annuals(p):
+#
+#     responses = []
+#     for c in COUNTY_LIST:
+#         county=c
+#         param=p
+#         r = requests.get(COUNTY_ANNUAL_SUMMARY_URL+f"&county={county}")
